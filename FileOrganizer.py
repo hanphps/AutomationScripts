cc5102ce2f2d197a8e982043d8a99921ffbe3c7c
Hannah <19543022+hanphps@users.noreply.github.com>
2020-07-13 01:30:26 -0400

    Update FileOrganizer.py
    ADDED: 
    TestCase2: includes testing directory checker and file mover

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,22 +1,34 @@
 import os, shutil
 from pathlib import Path
 
+"""
+    TODO:
+        - Make directories for nulls.
+        - Task scheduler
+        - DONE?
+"""
+
 HOME = str(Path.home())
 DOWNLOADS = Path(HOME+'/Downloads')
-#MAIN = Path('hannah/Documents/Library')
+MAIN = Path(HOME+'/Documents/Library')
 
+"""
+    Rules for moving files
+        FRONT takes front part of name and matches it to specific directory with keywords
+        END takes end extensions and matches to specific directory
+"""
 RULES = {
     'front': {
-        'BOOK' : {'Books'},
-        'SCHOOL' : {'School'},
-        'WORK' : {'Work'},
-        'PROJECT': {'Projects'}
+        'BOOK' : 'Books',
+        'SCHOOL' : 'School',
+        'WORK' : 'Work',
+        'PROJECT': 'Projects'
     },
 
     'end':{
-        '.pdf.ocx' : {'Documents'},
-        '.png.jpg.mp4':{'Memes'},
-        '.exe' : {'Installers'}
+        '.pdf.ocx' : 'Documents',
+        '.png.jpg.mp4': 'Memes',
+        '.exe' : 'Installers'
     }
 }
 
@@ -25,27 +37,49 @@ def detectRule(file):
         for i,v in dic.items():
             if location == 'front':
                 if i in file:
-                    print(v) #change to return later
+                    return v
             
             elif location == 'end':
                 end = file[-3:]
                 types = i.split('.')
                 for e in types:
                     if end == e:
-                        print(v) #change to return later
+                        return v
 
             else:
-                print('error') #change to return NULL later         
+                print('error') #change to return NULL later 
+                return None       
+
+def checkDestinationExist(name):
+    if not(MAIN is None):
+        if name in (os.listdir(MAIN)):
+            return True  
+    else:
+        return False
 
 
-#def mover(file):
-    #TODO make it!
 
+def mover(file):
+    file = str(file)
+    directory = detectRule(file)
+    if checkDestinationExist(directory) == True:
+        directory = Path(str(MAIN)+'/'+directory)
+        shutil.move(file,directory)
+    else:
+        #Need to throw/catch some stuff
+        pass
 
-def TestCase():
+def TestCase1():
     for f in DOWNLOADS.iterdir():
         f = str(f)
         print(f)
         detectRule(f)
 
-TestCase()
+def TestCase2():
+    TestFolder = Path(str(DOWNLOADS)+'/TestFiles')
+    for f in TestFolder.iterdir():
+        mover(f)
+
+
+#TestCase1() PASS
+#TestCase2() PASS
